c6149c6480a1181e9e63566e39bb3520fdc9d2fe
JuhaszZoltan <juhaszzoltanmate@yahoo.com>
2020-04-15 11:38:04 +0200

    oravege

---
+++ WFA200408A/FrmMain.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace WFA200408A
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.vEVŐKToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kERESŐToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Font = new System.Drawing.Font("Segoe UI", 16F);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.vEVŐKToolStripMenuItem,
+            this.kERESŐToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(709, 45);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // dgv
+            // 
+            this.dgv.AllowUserToAddRows = false;
+            this.dgv.AllowUserToDeleteRows = false;
+            this.dgv.AllowUserToResizeColumns = false;
+            this.dgv.AllowUserToResizeRows = false;
+            this.dgv.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgv.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgv.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dgv.Location = new System.Drawing.Point(12, 48);
+            this.dgv.Name = "dgv";
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgv.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dgv.RowHeadersVisible = false;
+            this.dgv.RowHeadersWidth = 51;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.dgv.RowsDefaultCellStyle = dataGridViewCellStyle3;
+            this.dgv.RowTemplate.Height = 24;
+            this.dgv.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgv.Size = new System.Drawing.Size(685, 390);
+            this.dgv.TabIndex = 1;
+            // 
+            // vEVŐKToolStripMenuItem
+            // 
+            this.vEVŐKToolStripMenuItem.ForeColor = System.Drawing.Color.Red;
+            this.vEVŐKToolStripMenuItem.Name = "vEVŐKToolStripMenuItem";
+            this.vEVŐKToolStripMenuItem.Size = new System.Drawing.Size(114, 41);
+            this.vEVŐKToolStripMenuItem.Text = "VEVŐK";
+            // 
+            // kERESŐToolStripMenuItem
+            // 
+            this.kERESŐToolStripMenuItem.ForeColor = System.Drawing.Color.Green;
+            this.kERESŐToolStripMenuItem.Name = "kERESŐToolStripMenuItem";
+            this.kERESŐToolStripMenuItem.Size = new System.Drawing.Size(125, 41);
+            this.kERESŐToolStripMenuItem.Text = "KERESŐ";
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "típus";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "ár";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "állapot";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(709, 450);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FrmMain";
+            this.Text = "USEDUFO.glx kínálata";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem vEVŐKToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kERESŐToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+    }
+}
+
---
+++ WFA200408A/FrmMain.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFA200408A
+{
+    public partial class FrmMain : Form
+    {
+        SqlConnection conn;
+
+        public FrmMain()
+        {
+            AppDomain.CurrentDomain.SetData(
+                "DataDirectory",
+                Path.GetDirectoryName(
+                    Assembly.GetExecutingAssembly()
+                    .Location)
+                .Replace(@"bin\Debug", "Res"));
+
+            conn = new SqlConnection(
+                @"Server=(localdb)\MSSQLLocalDB;" +
+                @"AttachDbFilename=|DataDirectory|hasznaltufo.mdf;");
+
+            InitializeComponent();
+            this.Icon = Properties.Resources.ufo;
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            conn.Open();
+
+            var cmd = new SqlCommand(
+                "SELECT tipus.nev, tipus.alapar, ufo.lopott, ufo.serules " +
+                "FROM ufo INNER JOIN tipus ON ufo.tipus = tipus.tipusid;", conn);
+
+            var r = cmd.ExecuteReader();
+
+            while (r.Read())
+            {
+                dgv.Rows.Add(
+                    //név
+                    r[0],
+                    //ár
+                    Math.Round((r.GetInt32(1) - ((r.GetInt32(1) / 100F) * r.GetInt32(3)))
+                    * (r.GetBoolean(2) ? 0.8 : 1)) + " CP",
+                    //állapot
+                    r[3]);
+            }
+
+            conn.Close();
+        }
+    }
+}
